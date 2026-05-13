393930e5cd781e8ef102e8920ee7721f7321e47c
andres0h5 <andresfelipr396@gmail.com>
2026-05-12 21:32:41 -0500

    Delete incluir directory

--- incluir/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuración de la base de datos
-$servidor = "localhost";
-$usuario  = "root";
-$password = "";
-$base_de_datos = "db-liceo-maranata"; 
-$conexion = mysqli_connect($servidor, $usuario, $password, $base_de_datos);
-?>
